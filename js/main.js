59649f0003319747ade220e16d5bdc103b219f3a
Juan Cruz Cagnoni <juancagnoni@gmail.com>
2023-06-28 12:13:40 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,283 @@
+const div = document.getElementById("response");
+const main = document.getElementById("lastNew");
+const section = document.getElementById("section");
+const modal = document.getElementById("modal");
+const favoritesMenu = document.getElementById("favorites");
+const modalFavorites = document.getElementById("modalFavorites");
+
+let favorites = [];
+
+// Notificaciones
+function showNotificationConfirmation() {
+  const confirmation = confirm("¿Deseas recibir notificaciones?");
+  if (confirmation) {
+    // El usuario aceptó recibir notificaciones, solicitar permiso.
+    Notification.requestPermission().then(function (permission) {
+      if (permission === "granted") {
+        // Permiso concedido, enviar la notificación.
+        sendNotification();
+      }
+    });
+  }
+}
+
+function sendNotification() {
+  const notificationTitle = "¡Bienvenido a LastNews!";
+  const notificationOptions = {
+    body: "Gracias por visitarnos.",
+    icon: "img/icon-192x192.png",
+  };
+  showNotification(notificationTitle, notificationOptions);
+}
+
+function showNotification(title, options) {
+  // Verificar si el navegador admite las notificaciones.
+  if ("Notification" in window) {
+    // Verificar el estado del permiso de notificación.
+    if (Notification.permission === "granted") {
+      // Mostrar la notificación.
+      new Notification(title, options);
+    }
+  }
+}
+
+function handleOnline() {
+  const status = document.getElementById("status");
+  // Realizar los cambios estéticos para el estado en línea
+  status.classList.remove("offline");
+  status.classList.add("online");
+  console.log("Estás en línea");
+}
+
+function handleOffline() {
+  const status = document.getElementById("status");
+  // Realizar los cambios estéticos para el estado sin conexión
+  status.classList.remove("online");
+  status.classList.add("offline");
+  console.log("Estás sin conexión");
+}
+
+window.addEventListener("DOMContentLoaded", function () {
+  // Registramos service worker.
+  if ("serviceWorker" in navigator) {
+    this.navigator.serviceWorker
+      .register("sw.js")
+      .then((res) => console.log("El SW se registro correctamente."))
+      .catch((err) => console.log("El SW no se pudo registrar"));
+  }
+
+  // Verificar si el navegador admite las notificaciones.
+  if ("Notification" in window) {
+    // Verificar el estado del permiso de notificación.
+    if (Notification.permission === "granted") {
+      // Si el permiso ya está concedido, puedes enviar la notificación directamente.
+      sendNotification();
+    } else if (Notification.permission !== "denied") {
+      // Si el permiso no se ha solicitado previamente, mostrar una confirmación.
+      showNotificationConfirmation();
+    }
+  }
+
+  // Verificar el estado de conexión al cargar la página
+  if (navigator.onLine) {
+    // Estás en línea
+    handleOnline();
+  } else {
+    // Estás sin conexión
+    handleOffline();
+  }
+
+  // Agregar event listeners para los eventos online y offline
+  window.addEventListener("online", handleOnline);
+  window.addEventListener("offline", handleOffline);
+
+  // Traemos la API.
+  const url =
+    "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=65dbea689e6a43a5866bd6f451512edd";
+
+  // Realizamos la solicitud Fetch para recuperar el archivo JSON.
+  fetch(url)
+    // Convertimos la respuesta JSON en un objeto JavaScript.
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+
+      // Obtener la cadena JSON de las noticias
+      const newsData = JSON.stringify(data.articles);
+
+      // Guardar la cadena JSON en el LocalStorage
+      localStorage.setItem("newsData", newsData);
+
+      const storedNewsData = localStorage.getItem("newsData");
+
+      if (storedNewsData) {
+        // Convertir la cadena JSON de vuelta a un array de objetos JavaScript
+        const storedData = JSON.parse(storedNewsData);
+
+        // Listado de noticias.
+        for (const article of storedData) {
+          // Creamos la tarjeta de la noticia.
+          let cardContainer = document.createElement("div");
+          cardContainer.className = "new";
+
+          cardContainer.innerHTML = `
+          <h3 href="">${article.title}</h3>
+          <p>${article.description}</p>
+        `;
+
+          let cardContainerBottom = document.createElement("div");
+          cardContainerBottom.className = "card-container-bottom";
+
+          let published = this.document.createElement("p");
+          published.innerHTML = `${article.publishedAt}`;
+
+          // Creamos el boton para ver el detalle de la noticia en una ventana modal.
+          let linkNew = document.createElement("a");
+          linkNew.className = "link";
+          linkNew.innerHTML = "See more";
+          linkNew.addEventListener("click", () => {
+            modal.style.display = "flex";
+
+            // Detalle de la noticia.
+            let containerNew = document.createElement("div");
+            containerNew.classList.add("container-new", "row", "pt-4");
+            containerNew.innerHTML = `
+
+            <div class="p-3 col-12 col-md-6 mt-5">
+              <h3 class="title" href="">${article.title}</h3>
+              <p class="description">${article.description}</p>
+              <p class="contentNew">${article.content}</p>
+              <p class="author">Author: ${article.author}</p>
+              <p class="dateNew mt-5">${article.publishedAt}</p>
+            </div>
+          
+          `;
+
+            let imgContainer = this.document.createElement("div");
+            imgContainer.classList.add("p-3", "col-12", "col-md-6", 'mt-5', 'd-flex', 'flex-column', 'align-items-center');
+
+            let img = this.document.createElement("img");
+            img.src = `${article.urlToImage}`;
+            img.classList.add('imgNew', 'img-fluid');
+
+            // Div para los botones
+            let containerButtons = document.createElement("div");
+            containerButtons.className = "container-buttons";
+
+            // Creamos el boton para agregar a favoritos.
+            let favoritesButton = document.createElement("a");
+            favoritesButton.className = "favorites-button";
+            favoritesButton.innerHTML =
+              '<i class="bi bi-star"></i> Add to favorites';
+            favoritesButton.addEventListener("click", () => {
+              favorites.push(article);
+              console.log(favorites);
+            });
+
+            // Creamos el boton para compartir
+            const shareButton = document.createElement("a");
+            shareButton.className = "share-button";
+            shareButton.innerHTML = "Share";
+
+            shareButton.addEventListener("click", () => {
+              if (navigator.share) {
+                navigator
+                  .share({
+                    title: "Título de tu contenido",
+                    text: "Texto de tu contenido",
+                    url: "URL de tu contenido",
+                  })
+                  .then(() => {
+                    console.log("Contenido compartido con éxito.");
+                  })
+                  .catch((error) => {
+                    console.error("Error al compartir:", error);
+                  });
+              } else {
+                console.log(
+                  "La función de compartir no está soportada en este dispositivo o navegador."
+                );
+              }
+            });
+
+            // Boton para cerrar la ventana modal de la noticia.
+            let close = document.createElement("div");
+            close.className = "close";
+
+            let closeBtn = document.createElement("a");
+            closeBtn.href = "javascript:void(0)";
+            closeBtn.innerHTML = "Go Back";
+            closeBtn.addEventListener("click", () => {
+              containerNew.remove();
+              modal.style.display = "none";
+              return false;
+            });
+
+            close.append(closeBtn);
+            containerButtons.append(favoritesButton, shareButton);
+            imgContainer.append(img, containerButtons);
+            containerNew.append(close, imgContainer);
+            modal.append(containerNew);
+          });
+
+          cardContainerBottom.append(published, linkNew);
+          cardContainer.append(cardContainerBottom);
+          div.append(cardContainer);
+        }
+      }
+
+      // Creamos la ventana modal de los favoritos.
+      favoritesMenu.addEventListener("click", () => {
+        modalFavorites.style.display = "flex";
+
+        let containerFavorites = document.createElement("div");
+        containerFavorites.className = "container-favorites";
+        containerFavorites.innerHTML = "<h4>My favorite news</h4>";
+
+        favorites.forEach((favoriteNew) => {
+          let favoriteNewContainer = document.createElement("div");
+          favoriteNewContainer.classList.add(
+            "d-flex",
+            "align-items-center",
+            "mb-3"
+          );
+          favoriteNewContainer.innerHTML = `
+            <i class="bi bi-star-fill me-2"></i>
+            <p class="favorite-new mb-0 me-5">${favoriteNew.title}</p>
+          `;
+
+          // Boton para eliminar una noticia de favoritos.
+          let deleteNew = document.createElement("a");
+          deleteNew.innerHTML = '<i class="bi bi-trash me-3"></i>';
+          deleteNew.style.cursor = "pointer";
+          deleteNew.addEventListener("click", () => {
+            favoriteNewContainer.remove();
+            favorites.pop();
+          });
+
+          favoriteNewContainer.append(deleteNew);
+          containerFavorites.append(favoriteNewContainer);
+        });
+
+        // Boton para cerrar la ventana modal de los favoritos.
+        let close = this.document.createElement("div");
+        close.className = "close";
+
+        let closeBtn = document.createElement("a");
+        closeBtn.href = "javascript:void(0)";
+        closeBtn.innerHTML = "Close";
+        closeBtn.addEventListener("click", () => {
+          containerFavorites.remove();
+          modalFavorites.style.display = "none";
+          return false;
+        });
+
+        close.append(closeBtn);
+        containerFavorites.append(close);
+        modalFavorites.append(containerFavorites);
+      });
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+});
---
+++ sw.js
@@ -0,0 +1,62 @@
+const cacheName = "pwa-parcial-1-dwm3ap-cagnoni-juancruz";
+const assets = [
+  "index.html",
+  "bootstrap/css/bootstrap.min.css",
+  "bootstrap/js/bootstrap.min.js",
+  "css/style.css",
+  "js/main.js",
+  "manifest.json",
+  "img/icon-192x192.png",
+  "img/icon-256x256.png",
+  "img/icon-384x384.png",
+  "img/icon-512x512.png",
+  "img/logo.png",
+  "img/main.png",
+];
+
+self.addEventListener("install", (event) => {
+  console.log("El SW se instaló");
+  // Permite que el Service Worker tome el control de la aplicación de inmediato.
+  self.skipWaiting(); 
+
+  event.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      // Precachea los archivos especificados en el array 'assets'.
+      cache.addAll(assets); 
+    })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  console.log("El SW se activó");
+
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames
+        // Filtra los cachés antiguos que no coinciden con 'cacheName'.
+          .filter((name) => name !== cacheName) 
+          // Elimina los cachés antiguos en paralelo.
+          .map((name) => caches.delete(name)) 
+      );
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      if (response) {
+        // Si se encuentra una coincidencia en el caché, se devuelve la respuesta del caché.
+        return response; 
+      } else {
+        return fetch(event.request).catch(() => {
+          // Si no hay conexión a internet, se devuelve la página "index.html" desde el caché.
+          return caches.match("index.html"); 
+        });
+      }
+    })
+  );
+});
+
+
